4a7866860a2f051dddab2b503784d523e3b3d19a
Ibrahim <ibrahimghali211@gmail.com>
2022-09-26 17:36:02 +0300

    using cookies

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require('express-async-errors');
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
 
 const authRoutes = require('./routes/auth');
 const errorHandler = require('./middlewares/error');
@@ -21,10 +22,25 @@ app.use(express.static('public'));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get('/', (req, res) => res.status(200).render('home'));
 app.get('/smoothies', (req, res) => res.status(200).render('smoothies'));
 app.use(authRoutes);
+
+app.get('/set-cookie', (req, res) => {
+  res.cookie('isLogin', false, {
+    maxAge: 10000 * 60 * 60 * 24,
+    secure: true,
+    httpOnly: true,
+  });
+  res.send('done');
+});
+
+app.get('/get-cookie', (req, res) => {
+  const cookies = req.cookies;
+  res.json(cookies);
+});
 
 app.use(errorHandler);
 app.use((req, res) => {
