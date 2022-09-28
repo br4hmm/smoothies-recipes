20eb11e53130652940d53976af529a46475a5583
Ibrahim <ibrahimghali211@gmail.com>
2022-09-28 16:05:12 +0300

    add signup.ejs frontent validation

--- public/signup.js
+++ public/signup.js
@@ -1,13 +1,29 @@
 const form = document.querySelector('form');
+const emailError = document.querySelector('.email.error');
+const passwordError = document.querySelector('.password.error');
+
 form.addEventListener('submit', async event => {
   event.preventDefault();
 
   const email = form.email.value;
   const password = form.password.value;
+  emailError.textContent = '';
+  passwordError.textContent = '';
 
   const result = await fetch('/signup', {
     method: 'POST',
     body: JSON.stringify({ email, password }),
     headers: { 'Content-Type': 'application/json' },
   });
+
+  const data = await result.json();
+
+  console.log(data);
+
+  emailError.textContent = data.email;
+  passwordError.textContent = data.password;
+
+  if (data.user) {
+    location.assign('/');
+  }
 });
