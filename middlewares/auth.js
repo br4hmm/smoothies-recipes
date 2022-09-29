c7bf8fab7a3596f4a3e15c18b57383f0c32edacd
Ibrahim <ibrahimghali211@gmail.com>
2022-09-29 19:50:57 +0300

    setup app for production

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -16,10 +16,8 @@ const reqAuth = (req, res, next) => {
   if (token) {
     jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
       if (err) {
-        console.log(err.message);
         res.redirect('/login');
       } else {
-        console.log(decodedToken);
         next();
       }
     });
@@ -34,7 +32,6 @@ const checkUser = (req, res, next) => {
   if (token) {
     jwt.verify(token, process.env.JWT_SECRET, async (err, decodedToken) => {
       if (err) {
-        console.log(err.message);
         res.locals.user = null;
         next();
       } else {
