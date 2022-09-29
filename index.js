6fa21d4a262a14416bd8b76fa4da0b02d8c46c6e
Ibrahim <ibrahimghali211@gmail.com>
2022-09-29 19:46:58 +0300

    check the current user

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cookieParser = require('cookie-parser');
 
 const authRoutes = require('./routes/auth');
 const errorHandler = require('./middlewares/error');
-const { reqAuth } = require('./middlewares/auth');
+const { reqAuth, checkUser } = require('./middlewares/auth');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -25,6 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.get('*', checkUser);
 app.get('/', (req, res) => res.status(200).render('home'));
 app.get('/smoothies', reqAuth, (req, res) =>
   res.status(200).render('smoothies')
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
+const User = require('../models/User');
+
 // maxAge = 3 days
 const maxAge = 3 * 24 * 60 * 60;
 
@@ -26,4 +28,25 @@ const reqAuth = (req, res, next) => {
   }
 };
 
-module.exports = { maxAge, createToken, reqAuth };
+const checkUser = (req, res, next) => {
+  const token = req.cookies.jwt;
+
+  if (token) {
+    jwt.verify(token, process.env.JWT_SECRET, async (err, decodedToken) => {
+      if (err) {
+        console.log(err.message);
+        res.locals.user = null;
+        next();
+      } else {
+        let user = await User.findById(decodedToken.id);
+        res.locals.user = user;
+        next();
+      }
+    });
+  } else {
+    res.locals.user = null;
+    next();
+  }
+};
+
+module.exports = { maxAge, createToken, reqAuth, checkUser };
